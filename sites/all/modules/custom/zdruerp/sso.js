d720ceda4d9ab9983e240faeba92a026c5bcaf7a
dale <dale@daledeMacBook.local>
2016-04-05 16:27:44 +0800

    sso op

--- sites/all/modules/custom/zdruerp/sso.js
+++ sites/all/modules/custom/zdruerp/sso.js
@@ -1,12 +1,11 @@
 (function($){
   $("a[href*='user/logout']").addClass('SSOLOGOUT');
-  console.log($("a[href*='user/logout']").attr('href'));
-
   $("a[href*='user/logout']").on('click',function(e){
     e.preventDefault();
-    alert('clicked! first logout sso then logout drupal!');
-    // logout();//SSO logout!
+    console.log('clicked logout ,need call SSO logout!');
+    logout();//SSO logout!
+    console.log('SSO logout called then drupal logout!');
     window.location.href = $("a[href*='user/logout']").attr('href');
-    alert('if success! logout drupal!');
+    console.log('drupal logout!');
   });
 })(jQuery)
