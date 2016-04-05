95f919bc7885b07467c60716fc4a1f406e471b23
dale <dale@daledeMacBook.local>
2016-04-05 16:17:06 +0800

    no remotr

--- sites/all/themes/proedge/templates/pages/page--user--password.tpl.php
+++ sites/all/themes/proedge/templates/pages/page--user--password.tpl.php
@@ -1,23 +1,23 @@
 <!-- 页头 -->
 <div class="login_header">
   <div class="container">
-      <img src="http://wdev.camplus.cn:8003/sites/all/themes/proedge/images/login/logo_text.png" class="pull-left">
+      <img src="/sites/all/themes/proedge/images/login/logo_text.png" class="pull-left">
     <div class="back_link floatRight">
-      <a href="<?php print url('<front>'); ?>"><img src="http://wdev.camplus.cn:8003/sites/all/themes/proedge/images/login/home.png"><?php print t('返回首页'); ?></a>
+      <a href="<?php print url('<front>'); ?>"><img src="/sites/all/themes/proedge/images/login/home.png"><?php print t('返回首页'); ?></a>
     </div>
   </div>
 </div>
 <!-- 主体 -->
 <div class="RePassword">
   <div id="logoBg">
-    <img src="http://wdev.camplus.cn:8003/sites/all/themes/proedge/images/login/logoBg.jpg" width="1920" height="739">
+    <img src="/sites/all/themes/proedge/images/login/logoBg.jpg" width="1920" height="739">
   </div>
   <div class="RePasswordBody">
     <div id="auth_box" class="password">
       <div id="middle_part">
         <h2 class="title"><?php print $title; ?></h2>
         <?php print $messages; ?>
-        
+
         <?php print render($page['content']); ?>
       </div>
       <div id="bottom_part">
@@ -52,7 +52,7 @@
                     </row>
                     <row>
                         <div class="col-lg-12 col-md-12 col-sm-12 logo">
-                            <div style="float: left; padding-right: 20px; padding-top: 10px;"><img width="77" height="49" src="http://wdev.camplus.cn:8003/sites/all/themes/proedge/images/login/logo_white.png"></div>
+                            <div style="float: left; padding-right: 20px; padding-top: 10px;"><img width="77" height="49" src="/sites/all/themes/proedge/images/login/logo_white.png"></div>
                             <div style="float: left;">
                                 <p>Copyright @2013-2016 Qcloud.com.&nbsp;科服驿站&nbsp;版权所有</p>
                                 <p>绵阳科技城科技服务有限责任公司&nbsp;版权所有&nbsp;&nbsp;ICP备案许可证:川ICP备&nbsp;11018762号</p>
@@ -68,7 +68,7 @@
                     <p>客服邮箱：kefu@wfyizhan.com</p>
                 </div>
                 <div class="col-lg-2 col-md-2 col-sm-2">
-                    <div><img src="http://wdev.camplus.cn:8003/sites/all/themes/proedge/images/login/microcode.png"></div>
+                    <div><img src="/sites/all/themes/proedge/images/login/microcode.png"></div>
                     <div style="font-size: 16px; padding-top: 5px;">微信扫我得服务券</div>
                 </div>
             </div>
